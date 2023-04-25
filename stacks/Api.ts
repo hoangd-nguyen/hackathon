f214fe6909630e562a3fb553f02323710ccac85f
Hoang Duong Nguyen <hoang.duongnguyen@di-on.solutions>
2023-04-25 15:11:29 +0200

    Remove deprecated imports

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -1,21 +1,7 @@
 import { Api as ApiGateway, StackContext, use } from "sst/constructs";
 
-import { Auth } from "./Auth";
-import { getCustomDomainConfig } from "./Domain";
-
 export function Api(context: StackContext) {
-  const cognito = use(Auth);
   const api = new ApiGateway(context.stack, "api", {
-    customDomain: getCustomDomainConfig(context, "api"),
-    authorizers: {
-      Authorizer: {
-        type: "user_pool",
-        userPool: {
-          id: cognito.userPoolId,
-          clientIds: [cognito.userPoolClientId],
-        },
-      },
-    },
     cors: {
       // TODO: Refine CORS config!
       allowMethods: ["GET", "POST", "PUT", "DELETE"],
@@ -26,8 +12,6 @@ export function Api(context: StackContext) {
       authorizer: "Authorizer",
     },
   });
-
-  cognito.attachPermissionsForAuthUsers(context.stack, [api]);
 
   context.stack.addOutputs({
     ApiEndpoint: api.url,
