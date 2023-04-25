ba118c730fd04c6999ecb705786298f3a5d09408
Hoang Duong Nguyen <hoang.duongnguyen@di-on.solutions>
2023-04-25 15:14:18 +0200

    Remove deprecated imports

--- stacks/WebEnv.ts
+++ stacks/WebEnv.ts
@@ -2,19 +2,15 @@ import * as ssm from "aws-cdk-lib/aws-ssm";
 import { StackContext, use } from "sst/constructs";
 
 import { Api } from "./Api";
-import { Auth } from "./Auth";
 import { getSsmPrefix } from "./utils/getSsmPrefix";
 
 export function WebEnv(context: StackContext) {
   const ssmNamePrefix = getSsmPrefix(context);
   const api = use(Api);
-  const cognito = use(Auth);
 
   const environmentVars = {
     VITE_APP_API_URL: api.url,
     VITE_APP_REGION: context.app.region,
-    VITE_APP_USER_POOL_ID: cognito.userPoolId,
-    VITE_APP_USER_POOL_CLIENT_ID: cognito.userPoolClientId,
   };
 
   return Object.entries(environmentVars).map((entry) => {
